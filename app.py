48245515f1093ae52035c3ede95006a7b2caa01f
toto111413 <tom77.puls@gmail.com>
2025-08-11 09:30:09 +0200

    Update app.py

--- app.py
+++ app.py
@@ -60,7 +60,8 @@ st.header("🎮 Mes jeux externes")
 jeux = [
     {"titre": "cible", "desc": "A tu fais le meilleure score ? tire vite.", "lien": "https://zmwguswsyytnolqexffdfj.streamlit.app/"},
     {"titre": "RPG", "desc": "Tuez le !", "lien": "https://je7erdurjykggnaagdzyzt.streamlit.app/"},
-    {"titre": "Quiz", "desc": "répondez !!!", "lien": "https://hyu2irxjzdthppfbix6duf.streamlit.app/"}
+    {"titre": "Quiz", "desc": "répondez !!!", "lien": "https://hyu2irxjzdthppfbix6duf.streamlit.app/"},
+    {"titre": "Dé", "desc": "faites un plus grand nombre", "lien": "https://essaie-2-hcaltzcmtgndkwfuei7snk.streamlit.app/"}
 ]
 for j in jeux:
     st.subheader(j["titre"])
